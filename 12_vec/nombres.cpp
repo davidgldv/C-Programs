02b5ef0ed324ae01666514cdbe42fe8792e65ac1
davidgldv <davidgldv@gmail.com>
2018-03-01 10:21:33 +0100

    Mejora

--- 12_vec/nombres.cpp
+++ 12_vec/nombres.cpp
@@ -14,17 +14,20 @@ int main () {
 	printf("¿Cuantos nombres quieres?:");
 	scanf(" %i", &veces);
 
+	l = (char **) realloc(l,veces*sizeof(char *));
+
 	for( int c = 0 ; c< veces ; c++){
 		printf("Nombre: ");
 		scanf(" %[^\n]", nombre);
 		len = strlen(nombre);
-		l = (char **) realloc(l,(1+c)*sizeof(char *));
+		*(l+c) = (char *) malloc ((len + 1)*sizeof(char*));
+		strncpy(*(l+c), nombre , len);
+        }
 
-		*(l+c) = (char *) realloc (*l,(len + 1)*sizeof(char*));
-		strncpy(*l, nombre , N);
-		printf("Hola, %s\n", *(l+c));
+	for ( int c =0 ; c < veces ; c++){
+                printf("Hola, %s\n", *(l + c));
+        }
 
-	}
 
 	for (int c= 0 ; c < veces ; c++)
 		free(*(l+c));
